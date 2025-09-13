1babd3a40dca10d178cea621c7a2cd70db304a3a
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-09-12 23:18:15 -0600

    week-2 folder, page.js file, and student-info.js file created and add some code as instructions.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+const StudentInfo = () => {
+  return (
+    <dive>
+      <h1> Sheeba Hussaini </h1>
+      <p>
+        <Link href="https://github.com/S-hussaini" target="_blank"> 
+          My GitHub Repository
+        </Link>
+      </p>
+    </dive>
+  );
+};
+export default StudentInfo;
