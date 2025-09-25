54283fa5cd8d4ae7e9cdf38ce13236ee67e23675
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-09-24 18:23:48 -0600

    small update in styling

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,11 +1,11 @@
-import react from "react";
+import React from "react";
 import ItemList from "./item-list";
 
 export default function Page(){
   return(
     <main className =" p-6 bg-gray-100 min-h-screen flex flex-col items-center">
       <h1 className = "text-3xl font-bold mb-6 text-gray-800">
-        Shoping list
+        Shopping List
       </h1>
       <div>
         <ItemList/>
