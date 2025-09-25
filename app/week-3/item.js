5efeb3273e2abd360b02601f1e4e44268c2ad92e
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-09-24 18:22:28 -0600

    in page.js i capitalize L in list

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,9 +3,9 @@ import React from "react";
 const Item = ({ name, quantity, category }) => {
   return (
     <li className="bg-gray-50 border border-gray-300 rounded-lg p-4 mb-4 shadow-sm list-none w-[400px]">
-      <strong className="text-gray-900 text-lg">{name}</strong>
+      <div className="text-gray-900 text-lg">{name}</div>
       <div className="text-gray-800 mt-1">Quantity: {quantity}</div>
-      <div className="text-gray-800 mt-1">Category: {category}</div>
+      <div className="text-gray-800 mt-1">category: {category}</div>
     </li>
   );
 };
