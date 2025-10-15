2cc1bbb5f2769febf2ce7105ae8209ba7c4511bf
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-10-15 15:37:25 -0600

    i have add the logic code for the litem-list.js and it is working as i want

--- app/week-6/item-list.js
+++ app/week-6/item-list.js
@@ -1,6 +1,51 @@
 "use client";
-
-import React, { useState } from "react";
+import React from "react";
 import Item from "./item";
+import { useState } from "react";
 import items from "./items.json";
+
+//Use the useState hook to create a state variable sortBy and its setter function setSortBy. This will be used to determine the sorting preference of the user.
+export default function ItemList() {
+  const [sortBy, setSortBy] = useState("name"); 
+
+  const sortItems = [...items].sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else if (sortBy === "category") {
+      return a.category.localeCompare(b.category);
+    } 
+    return 0;
+  });
+
+
+
+
+  return (
+    <main>
+      <div className ="flex gap-4 mb-6">
+        <button
+          onClick = {() => setSortBy ("name")}
+          className = {`px-4 py-2 rounded ${sortBy === "name" ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"}`}>
+          Sort by Name
+        </button>
+        <button
+          onClick = {() => setSortBy ("category")}
+          className = {`px-4 py-2 rounded ${sortBy === "category" ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-800"}`}>
+          Sort by Category
+          </button>
+      </div>
+
+      <ul>
+        {sortItems.map((item) =>(
+          <Item
+          key = {item.id}
+          name = {item.name}
+          quantity = {item.quantity}
+          category = {item.category}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+};
 
