f53d8b41c75cc2d4888f0ca2e45d04c2c96d7743
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-10-23 10:40:40 -0600

    And i have update the page.js according to the assignment

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,19 +1,27 @@
-import React from "react";
-import ItemList from "../week-7/item-list";
+"use client";
+import { useState } from "react";
+import ItemList from "./item-list";
 import NewItem from "./new-item";
-import items from "./items.json";
+import itemsData from "./items.json";
 
 
+//Initialize a state variable (e.g., items) with the data from items.json.
 
-export default function Page(){
-  return(
-    <main className =" p-6 bg-gray-100 min-h-screen flex flex-col items-center">
+export default function Page()
+{
+  const [items, setItems] = useState(itemsData);
+
+  const handelAddItem = (newItem) => {
+    setItems((prevItems) =>[...prevItems, newItem]);
+  };
+
+  return (
+    <main className = " p-6 bg-gray-100 min-h-screen flex flex-col items-center">
       <h1 className="text-4xl font-bold mb-10 text-gray-900 tracking-tight drop-shadow-sm">
-        Shopping List
+        Week 7 --- Shopping List
       </h1>
-      <div>
-        <ItemList/>
-      </div>
+      <NewItem onAddItem={handelAddItem} />
+      <ItemList items={items} />
     </main>
   );
 }
