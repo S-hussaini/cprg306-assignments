48ab7f214798fdcd5fa42b25ed666b0eb3983267
S-hussaini <Sheeba.Hussaini@edu.sait.ca>
2025-10-30 22:40:57 -0600

    i have change and add some code in this page to make it work as i want. i have Add onClick Handler to List Item.

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,9 +1,12 @@
 "use client";
 import React from "react";
 
-const Item = ({ name, quantity, category }) => {
+const Item = ({ name, quantity, category, onSelect}) => {
   return (
-    <li className="bg-gray-50 border border-gray-300 rounded-lg p-4 mb-4 shadow-sm list-none w-[400px]">
+    <li 
+      onClick={onSelect}
+      className="bg-gray-50 border border-gray-300 rounded-lg p-4 mb-4 shadow-sm list-none w-[400px] cursor-pointer hover:bg-gray-100"
+    >
       <div className="text-gray-900 text-lg">{name}</div>
       <div className="text-gray-800 mt-1">Quantity: {quantity}</div>
       <div className="text-gray-800 mt-1">category: {category}</div>
